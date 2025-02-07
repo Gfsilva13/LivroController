056bd8360ba948489c2d98e970ab7d6458feea72
genivaldo Ferreira <genivaldo13@gmail.com>
2025-02-07 01:07:49 -0300

    Adicionar arquivos de projeto.

---
+++ Models/Livro.cs
@@ -0,0 +1,13 @@
+﻿namespace LivroController.Models
+{
+    public class Livro
+    {
+      public int Id { get; set; }  // Chave primária
+      public string Titulo { get; set; }
+      public string Autor { get; set; }
+      public int AnoPublicacao { get; set; }
+      public string Genero { get; set; }
+
+      public String Editora { get; set; }
+    }
+}
---
+++ Models/LivroDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace LivroController.Models
+{
+    public class LivroDbContext : DbContext 
+    {
+        public DbSet<Livro> Livros { get; set; }
+        public LivroDbContext(DbContextOptions<LivroDbContext> options) : base(options)
+        {
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // Definir a string de conexão com o banco de dados do Azure
+            optionsBuilder.UseSqlServer("(LocalDB)\\MSSQLLocalDB\Biblioteca");
+        }
+    }
+}
